28ff47ef7a7f5c2aa9efce65ac8fd278900720b6
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-09-17 22:50:21 +0100

    user service impl created

--- src/main/java/com/security/spring/security/model/User.java
+++ src/main/java/com/security/spring/security/model/User.java
@@ -1,10 +1,17 @@
 package com.security.spring.security.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/security/spring/security/service/UserServiceImpl.java
+++ src/main/java/com/security/spring/security/service/UserServiceImpl.java
@@ -1,4 +1,47 @@
 package com.security.spring.security.service;
 
-public class UserServiceImpl {
+import com.security.spring.security.model.Role;
+import com.security.spring.security.model.User;
+import com.security.spring.security.repo.RoleRepo;
+import com.security.spring.security.repo.UserRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Service @Transactional @Slf4j
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
 }
